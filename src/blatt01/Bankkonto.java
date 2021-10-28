af1266a7317cba726efc871b25917d185ed44a3f
danisltpi <lcdns@protonmail.com>
2021-10-28 10:03:53 +0200

    added all tms

--- src/blatt01/Bankkonto.java
+++ src/blatt01/Bankkonto.java
@@ -13,7 +13,6 @@ public class Bankkonto {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		double guthabenInEuro = 0.0;
 		System.out.println("Guthaben = " + guthabenInEuro);
 		guthabenInEuro = guthabenInEuro + 100.0;
--- src/blatt02/Konto.java
+++ src/blatt02/Konto.java
@@ -8,27 +8,64 @@ public class Konto {
 		double zins = 2.0;
 		double guthaben;
 		final double ersparnis = 1000;	
-		int jahre = 30;
+		int jahre = 0;
+		double betrag = 1000;
+		
+
+		
 		
 		// Befragung
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Bitte geben Sie Ihr Startguthaben ein: ");
-		guthaben = sc.nextDouble();
-		System.out.println("Bitte geben Sie den Zinssatz(in %) ein: ");
-		zins = sc.nextDouble();
-		System.out.println("Bitte geben Sie die Laufzeit (in Jahren) ein: ");
-		jahre = sc.nextInt();
-
-		System.out.println(guthaben);
 		
-		for (int i=1; i < (jahre+1); i++) {	
-			System.out.println("Guthaben nach dem " + i + ". Jahr: " + guthaben);	
-			guthaben += guthaben * (zins * 0.01);
-			guthaben += ersparnis;	
+		System.out.println("Wähle 1 um einen Sparplan mit Zinseszins zu berechnen");
+		System.out.println("Wähle 2 um einen Sparplan ohne Zinseszins zu berechnen");	
+		System.out.println("Wähle 3 um die Erreichung eines Sparplans mit Zinseszins zu berechnen");
+		int auswahl = sc.nextInt();		
+	
+		
+
+		
+		//TM1
+		if (auswahl == 1) {	
+			System.out.println("Bitte geben Sie Ihr Startguthaben ein: ");
+			guthaben = sc.nextDouble();
+			System.out.println("Bitte geben Sie den Zinssatz(in %) ein: ");
+			zins = sc.nextDouble();
+			System.out.println("Bitte geben Sie die Laufzeit (in Jahren) ein: ");
+			jahre = sc.nextInt();
+			for (int i=1; i < (jahre+1); i++) {	
+				System.out.println("Guthaben nach dem " + i + ". Jahr: " + guthaben);	
+				guthaben += ersparnis + guthaben * (zins * 0.01);
+			}
 		}
-		for (int i=1; i<=30; i++) {
-			System.out.println("Guthaben nach dem " + i + ". Jahr: " + guthaben);
-			
+		//TM2
+		if (auswahl == 2) {
+			System.out.println("Bitte geben Sie Ihr Startguthaben ein: ");
+			guthaben = sc.nextDouble();
+			System.out.println("Bitte geben Sie den Zinssatz(in %) ein: ");
+			zins = sc.nextDouble();
+			System.out.println("Bitte geben Sie die Laufzeit (in Jahren) ein: ");
+			jahre = sc.nextInt();
+			for (int i=1; i<=jahre; i++) {
+				System.out.println("Guthaben nach dem " + i + ". Jahr: " + betrag);		
+				betrag = (betrag + ersparnis) + ersparnis * i * 0.01 * zins;
+				}
 		}
+		//TM3
+		if (auswahl == 3) {				
+			System.out.println("Bitte geben Sie Ihr Startguthaben ein: ");
+			guthaben = sc.nextDouble();
+			System.out.println("Bitte geben Sie den Zinssatz(in %) ein: ");
+			zins = sc.nextDouble();
+			System.out.println("Bitt geben Sie Ihr Sparziel ein: ");
+			double sparziel = sc.nextDouble();
+			while (guthaben < sparziel) {
+				guthaben = guthaben + (guthaben * (zins * 0.01));
+				guthaben = guthaben + ersparnis;
+				jahre++;
+			}
+			System.out.println("Guthaben nach dem " + jahre + ". Jahr: " + guthaben);
+		}
+		
 	}
 }
