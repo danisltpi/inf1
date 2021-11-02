36a2f2acca683e7c6d398da5366c682ffaf4dd2d
danisltpi <lcdns@protonmail.com>
2021-11-02 08:27:54 +0100

    finished 3.10

--- src/blatt03/LoesungXYRaster.java
+++ src/blatt03/LoesungXYRaster.java
@@ -42,5 +42,39 @@ public class LoesungXYRaster extends JFrame {
 	 */
    public void meinRaster(Graphics g){
 	   // TODO
+	   // vertikale linien
+	   // feste y koordinaten:
+	   int y1 = this.getHeight() / 4;	   
+	   int y2 = this.getHeight() * 3 / 4;
+	   
+	   for (int i=0; i<=10; i++) {
+		   int rasterLaenge = this.getWidth() / 2; 
+		   int x = (this.getWidth() / 4) + (rasterLaenge * i) / 10;
+		   if (i % 2 == 0) {
+			   g.setColor(Color.black);
+		   }
+		   else {
+			   g.setColor(Color.gray);
+		   }
+		   g.drawLine(x, y1, x, y2);
+	   }
+	   
+	   // horizontale linien
+	   // feste x 
+	   int x1 = this.getWidth() / 4; 
+	   int x2 = this.getWidth() * 3 / 4;
+
+	   for (int i=0; i<=10; i++) {
+		   int rasterHoehe = this.getHeight() / 2; 
+		   int y = (this.getHeight() / 4) + (rasterHoehe * i) / 10;
+		   if (i % 2 == 0) {
+			   g.setColor(Color.black);
+		   }
+		   else {
+			   g.setColor(Color.gray);
+		   }
+		   g.drawLine(x1, y, x2, y);
+		   
+	   }
    } 
 }
