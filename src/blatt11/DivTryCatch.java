e31e68b998131f512477b2ae3ffaf5bf096531ca
danisltpi <lcdns@protonmail.com>
2021-12-27 13:53:02 +0100

    add div aufgabe

---
+++ src/blatt11/DivTryCatch.java
@@ -0,0 +1,21 @@
+package blatt11;
+
+public class DivTryCatch {
+
+	public static int div(int z, int n) {
+		int i = Integer.MAX_VALUE;
+		try {
+			i = z / n;
+		} catch(ArithmeticException e) {
+			System.err.println("Fehlergrund: " + e.getMessage());
+			e.printStackTrace(System.err);
+		}
+		return i;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(div(100, 0));
+	}
+
+}
